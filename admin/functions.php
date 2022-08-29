c8e7df9e678e383182346491ecc2c4d5998f18a6
zaman shovon <zaman.shovon33@gmail.com>
2022-08-29 19:24:59 +0600

    post view added

--- admin/functions.php
+++ admin/functions.php
@@ -1,9 +1,10 @@
 <?php
 
 
-function get_absolute_url() {
-    $array = explode('/',$_SERVER['REQUEST_URI']);
-    $get_abs_url = explode('.php',$array[count($array)-1])[0];
+function get_absolute_url()
+{
+    $array = explode('/', $_SERVER['REQUEST_URI']);
+    $get_abs_url = explode('.php', $array[count($array) - 1])[0];
     return $get_abs_url;
 }
 
@@ -11,27 +12,27 @@ function get_absolute_url() {
 function insert_categories()
 {
     global $connection;
-    if(isset($_POST['submit'])) {
+    if (isset($_POST['submit'])) {
         $title = $_POST['cat_title'];
         $insert = "insert into categories(cat_title) ";
         $insert .= "value('{$title}')";
         $insert_query = mysqli_query($connection, $insert);
-        if(!$insert_query) {
-            die("Query failed with ".mysqli_error($connection));
+        if (!$insert_query) {
+            die("Query failed with " . mysqli_error($connection));
         }
         header("Location:categories.php");
-      }
+    }
 }
 function edit_category()
 {
     global $connection;
-    if(isset($_POST['update'])) {
+    if (isset($_POST['update'])) {
         $id = $_POST['cat_id'];
         $title = $_POST['cat_title'];
         $delete = "update categories set cat_title = '{$title}' where cat_id= '{$id}'";
         $delete_query = mysqli_query($connection, $delete);
-        if(!$delete_query) {
-            die("Query failed with ".mysqli_error($connection));
+        if (!$delete_query) {
+            die("Query failed with " . mysqli_error($connection));
         }
         header("Location:categories.php");
     }
@@ -39,12 +40,12 @@ function edit_category()
 function delete_categories()
 {
     global $connection;
-    if(isset($_GET['delete'])) {
+    if (isset($_GET['delete'])) {
         $id = $_GET['delete'];
         $delete = "delete from categories where cat_id= '{$id}'";
         $delete_query = mysqli_query($connection, $delete);
-        if(!$delete_query) {
-            die("Query failed with ".mysqli_error($connection));
+        if (!$delete_query) {
+            die("Query failed with " . mysqli_error($connection));
         }
         header("Location:categories.php");
     }
--- admin/posts.php
+++ admin/posts.php
@@ -1,6 +1,8 @@
-<?php ?>
-<!DOCTYPE html>
-  <?php include "layouts/header.php"; ?>
+  <?php 
+    include "layouts/header.php";
+    $query = "select * from posts order by post_id desc";
+    $result = mysqli_query($connection, $query);
+  ?>
   <body>
     <!-- Layout wrapper -->
     <div class="layout-wrapper layout-content-navbar">
@@ -10,82 +12,86 @@
         <!-- / Menu -->
         <!-- Layout container -->
         <div class="layout-page">
-        <?php include "layouts/navbar.php"; ?>
+          <?php include "layouts/navbar.php"; ?>
           <!-- Content wrapper -->
           <div class="content-wrapper">
             <!-- Content -->
             <div class="container-xxl flex-grow-1 container-p-y">
               <div class="row">
-              <div class="card">
-                <h5 class="card-header">Posts Table</h5>
-                <div class="table-responsive text-nowrap">
-                  <table class="table table-dark">
-                    <thead>
-                      <tr>
-                        <th>Project</th>
-                        <th>Client</th>
-                        <th>Users</th>
-                        <th>Status</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <tbody class="table-border-bottom-0">
-                      <tr>
-                        <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>Angular Project</strong></td>
-                        <td>Albert Cook</td>
-                        <td>
-                          <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
-                            <li
-                              data-bs-toggle="tooltip"
-                              data-popup="tooltip-custom"
-                              data-bs-placement="top"
-                              class="avatar avatar-xs pull-up"
-                              title="Lilian Fuller"
-                            >
-                              <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle" />
-                            </li>
-                            <li
-                              data-bs-toggle="tooltip"
-                              data-popup="tooltip-custom"
-                              data-bs-placement="top"
-                              class="avatar avatar-xs pull-up"
-                              title="Sophia Wilkerson"
-                            >
-                              <img src="../assets/img/avatars/6.png" alt="Avatar" class="rounded-circle" />
-                            </li>
-                            <li
-                              data-bs-toggle="tooltip"
-                              data-popup="tooltip-custom"
-                              data-bs-placement="top"
-                              class="avatar avatar-xs pull-up"
-                              title="Christina Parker"
-                            >
-                              <img src="../assets/img/avatars/7.png" alt="Avatar" class="rounded-circle" />
-                            </li>
-                          </ul>
-                        </td>
-                        <td><span class="badge bg-label-primary me-1">Active</span></td>
-                        <td>
-                          <div class="dropdown">
-                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
-                              <i class="bx bx-dots-vertical-rounded"></i>
-                            </button>
-                            <div class="dropdown-menu">
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-edit-alt me-1"></i> Edit</a
-                              >
-                              <a class="dropdown-item" href="javascript:void(0);"
-                                ><i class="bx bx-trash me-1"></i> Delete</a
-                              >
+                <div class="card">
+                  <h5 class="card-header">Posts Table</h5>
+                  <div class="table-responsive text-nowrap">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>S/N</th>
+                          <th>Title</th>
+                          <th>Post Author</th>
+                          <!-- <th>Post Content</th> -->
+                          <th>Post Image</th>
+                          <th>Post Tags</th>
+                          <th>Post Comments</th>
+                          <th>Post Views</th>
+                          <th>Status</th>
+                          <th>Post Date</th>
+                          <th>Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody class="table-border-bottom-0">
+                        <?php
+                          $i = 1;
+                          while ($row = mysqli_fetch_assoc($result)) {
+                        ?>     
+                        <tr>                                             
+                          <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong><?= $i ?></strong></td>
+                          <td><?= $row['post_title'] ?></td>
+                          <td><?= $row['post_author'] ?></td>
+                          <!-- <td><small><?= $row['post_content'] ?></small></td> -->
+                          <td>
+                            <img src="<?= $row['post_image'] ?>" alt="user-avatar" class="d-block rounded" height="100" width="100" id="uploadedAvatar">
+                          </td>
+                          <td>
+                            <?php
+                              foreach (explode(',',$row['post_tags']) as $key => $value) {
+                                echo '<span class="badge rounded-pill bg-info">'.$value.'</span>';
+                              }
+                             ?>
+                          </td>
+                          <td><i class='bx bx-message-rounded-dots' ></i> <?= $row['post_comment_count'] ?></td>
+                          <td><i class='bx bxs-binoculars' ></i> <?= $row['post_view_count'] ?></td>
+                          <!-- <td>
+                            <ul class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+                              <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" class="avatar avatar-xs pull-up" title="Lilian Fuller">
+                                <img src="../assets/img/avatars/5.png" alt="Avatar" class="rounded-circle" />
+                              </li>
+                            </ul>
+                          </td> -->
+                          <td><span class="badge bg-label-primary me-1"><?= $row['post_status'] ?></span></td>
+                          <td><span class="badge bg-label-primary me-1">
+                            <?php 
+                              $date = new DateTime($row['post_date']); echo $date->format("d M,Y"); 
+                            ?>
+                          </span></td>
+                          <td>
+                            <div class="dropdown">
+                              <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                <i class="bx bx-dots-vertical-rounded"></i>
+                              </button>
+                              <div class="dropdown-menu">
+                                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-1"></i> Edit</a>
+                                <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
+                              </div>
                             </div>
-                          </div>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <br>
+                          </td>
+                        </tr>
+                        <?php
+                            $i++;
+                        } ?>                        
+                      </tbody>
+                    </table>
+                    <br>
+                  </div>
                 </div>
-              </div>
               </div>
             </div>
             <!-- / Content -->
@@ -104,4 +110,5 @@
     </div>
     <?php include "layouts/scripts.php" ?>
   </body>
-</html>
+
+  </html>
